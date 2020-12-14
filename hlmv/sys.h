3d104c565a5f2f93edf695698ebe78a2c14c02c7
ozxybox <kieferstashu@gmail.com>
2020-12-14 12:11:08 -0600

    Add HLMV

---
+++ hlmv/sys.h
@@ -0,0 +1,14 @@
+//========= Copyright � 1996-2005, Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#ifndef SYS_H
+#define SYS_H
+#pragma once
+
+extern void Sys_CopyStringToClipboard( const char *pOut );
+
+#endif // SYS_H
